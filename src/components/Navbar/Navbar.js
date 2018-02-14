a67c3b5f7b4d0639cef5c56819e8f607e0e01576
Elisheva Elbaz <elishevaelbaz1@gmail.com>
2018-02-14 18:40:45 -0500

    text flashes red when user loses

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -7,6 +7,8 @@ const Navbar = props => <nav className="navbar navbar-inverse navbar-fixed-top">
  <p className="title">Clicky Game</p>
 <h3 id="scoreDisplay">Current Score: {props.score}</h3>
           <h3 id="topScoreDisplay">Top Score: {props.highScore}</h3>
+          
+          <h3 id="message" className={props.message == "You chose incorrectly" ? "incorrect" : "correct"}>{props.message}</h3>
 
           </div>
           </nav>;
